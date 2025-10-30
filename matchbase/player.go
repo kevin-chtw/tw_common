992171b065b90b2b986dd1122233f08b951f3283
chentw <451151014@qq.com>
2025-10-30 16:32:44 +0800

    断线重连

--- matchbase/player.go
+++ matchbase/player.go
@@ -7,7 +7,8 @@ type Player struct {
 	Sub     any
 	Ctx     context.Context
 	ID      string // 玩家唯一ID
-	State   int32  //玩家状态 0-在线 1-离线，2-退出
+	Online  bool   // 玩家在线状态
+	Exit    bool   // 玩家是否退出
 	MatchId int32
 	TableId int32
 	Score   int64 // 玩家分数
@@ -19,22 +20,11 @@ func NewPlayer(sub any, ctx context.Context, id string, matchId, tableId int32, 
 		Sub:     sub,
 		Ctx:     ctx,
 		ID:      id,
-		State:   0, // 默认在线状态
+		Online:  true,
+		Exit:    false,
 		MatchId: matchId,
 		TableId: tableId,
 		Score:   score,
 	}
 	return p
-}
-
-func (p *Player) SetState(online bool) bool {
-	state := int32(0)
-	if !online {
-		p.State = 1 // 离线状态
-	}
-	if p.State == state {
-		return false
-	}
-	p.State = state
-	return true
 }
